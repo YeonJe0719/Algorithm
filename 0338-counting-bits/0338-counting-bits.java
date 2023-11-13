311693ee777d6a1d688116c59da16e56e2cae657
YeonJe0719 <100188162+YeonJe0719@users.noreply.github.com>
2023-11-13 17:14:52 +0900

    Time: 2 ms (83.35%), Space: 46 MB (91.86%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,21 +2,9 @@ class Solution {
     public int[] countBits(int n) {
         int[] ans = new int[n+1];
         for(int i = 0; i <= n; i++){
-            String binaryString = Integer.toBinaryString(i);
-            int countOnes = countOnes(binaryString);
-            ans[i] = countOnes;
-            // ans[i] = Integer.bitCount(i);
+            ans[i] = Integer.bitCount(i);
         }
         return ans;
     }
 
-    private static int countOnes(String binaryString) {
-    int count = 0;
-    for (char c : binaryString.toCharArray()) {
-        if (c == '1') {
-            count++;
-        }
-    }
-    return count;
-}
 }
