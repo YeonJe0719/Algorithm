aef80daa382184d850e4c78b265983495b2f1d18
YeonJe0719 <100188162+YeonJe0719@users.noreply.github.com>
2023-11-28 12:30:05 +0900

    Time: 46 ms (8.90%), Space: 55.6 MB (35.66%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -3,20 +3,25 @@ import java.util.Map;
 
 class Solution {
     public int maxOperations(int[] nums, int k) {
-         Arrays.sort(nums);
-        int count = 0;
-        int lp = 0, rp = nums.length-1;
-            while(lp < rp){
-                if(nums[lp]+nums[rp] == k){
-                    count++;
-                    lp++;
-                    rp--;
-                }else if(nums[lp]+nums[rp] > k){
-                    rp--;
-                }else{
-                    lp++;
-                }
+          int maxOperations = 0;
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        // Count the frequency of each number in the array
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Iterate through the array and perform operations
+        for (int num : nums) {
+            int complement = k - num;
+
+            // Check if the complement is present in the map and its frequency is positive
+            if (frequencyMap.getOrDefault(complement, 0) > 0) {
+                maxOperations++;
+                frequencyMap.put(complement, frequencyMap.get(complement) - 1);
             }
-        return count;
+        }
+
+        return maxOperations / 2;
     }
 }
