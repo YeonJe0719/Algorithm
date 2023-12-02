a11d8e3b1c66e8d1886729f3e5b21211b8646dc1
YeonJe0719 <100188162+YeonJe0719@users.noreply.github.com>
2023-12-02 16:16:55 +0900

    Time: 10 ms (93.85%), Space: 44 MB (98.25%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,30 +1,20 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-          if (word1.length() != word2.length()) {
+        int[]str1 = new int[26];
+        int[]str2 = new int[26];
+        for(char c: word1.toCharArray())
+            str1[c - 'a']++;
+        for(char c: word2.toCharArray()){
+            str2[c - 'a']++;
+        if(str1[c -'a']==0)
             return false;
         }
-
-        Map<Character, Integer> freq1 = new HashMap<>();
-        Map<Character, Integer> freq2 = new HashMap<>();
-        Map<Integer, Integer> countFreq1 = new HashMap<>();
-        Map<Integer, Integer> countFreq2 = new HashMap<>();
-
-        for (char ch : word1.toCharArray()) {
-            freq1.put(ch, freq1.getOrDefault(ch, 0) + 1);
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        for(int i=0; i<26; i++){
+            if (str1[i]!= str2[i])
+                return false;
         }
-
-        for (char ch : word2.toCharArray()) {
-            freq2.put(ch, freq2.getOrDefault(ch, 0) + 1);
-        }
-
-        for (int freq : freq1.values()) {
-            countFreq1.put(freq, countFreq1.getOrDefault(freq, 0) + 1);
-        }
-
-        for (int freq : freq2.values()) {
-            countFreq2.put(freq, countFreq2.getOrDefault(freq, 0) + 1);
-        }
-
-        return freq1.keySet().equals(freq2.keySet()) && countFreq1.equals(countFreq2);
+        return true;
     }
 }
